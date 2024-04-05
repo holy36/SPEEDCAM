e4df455661a90485ba8fb37ffa47e069c5b58b99
Liu <doan.quang.luu@tpiv.sun-asterisk.com>
2024-04-05 23:48:23 +0700

    ff

--- main2.py
+++ main2.py
@@ -56,9 +56,9 @@ class PhotoViewer(QtWidgets.QGraphicsView):
     def handle_pinch(self, gesture):
         scale_factor = gesture.scaleFactor()
         if(scale_factor>1):
-            self._zoom +=1
+            self._zoom +=(1-scale_factor)
         else:
-            self._zoom -=1
+            self._zoom -=(scale_factor-1)
         if self._zoom > 0:
             self.scale(scale_factor,scale_factor)
         else:
