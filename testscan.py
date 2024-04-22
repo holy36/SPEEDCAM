b2446553938fd1ff675f489d4c0ed609197406e5
Liu <doan.quang.luu@tpiv.sun-asterisk.com>
2024-04-22 16:48:01 +0700

    ze

--- testscan.py
+++ testscan.py
@@ -1,5 +1,4 @@
 import bluetooth
-from bluetooth import Protocols
 
 def search_devices():
     print("Đang tìm kiếm các thiết bị Bluetooth xung quanh...")
@@ -9,25 +8,9 @@ def search_devices():
         # Create a Bluetooth socket and connect to the target device
         print(f"{addr} - {name}")   
 
-def connect_to_device(device_address):
-    port = 1  # RFCOMM port number
-    try:
-        print(f"Đang kết nối đến thiết bị có địa chỉ {device_address}...")
-        sock = bluetooth.BluetoothSocket(Protocols.RFCOMM)
-        sock.connect((device_address, port))
-        print("Đã kết nối thành công!")
-        return sock
-    except Exception as e:
-        print(e)
-        return None
+
 # D8:3A:DD:BF:33:D1 - raspberrypi 
 def main():
     search_devices()
-    device_address = input("Nhập địa chỉ của thiết bị bạn muốn kết nối: ")
-    sock = connect_to_device(device_address)
-    if sock:
-        # Thực hiện các hoạt động sau khi kết nối thành công
-        pass
-
 if __name__ == "__main__":
     main()
